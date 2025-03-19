df3cf6c6d6bc2d14290707cb899f716984e9656f
mubinkg <mubin.ice.ru@gmail.com>
2025-03-19 19:45:03 +0600

    init project

---
+++ src/components/login-form.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { login } from "@/actions/auth"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center">Login to your account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={login}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="username"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full cursor-pointer">
+                  Login
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
---
+++ src/data/index.ts
@@ -0,0 +1,12 @@
+const users = [
+    {
+        userName: "admin",
+        password: 'admin',
+        role: "admin"
+    },
+    {
+        userName: "user",
+        password: "user",
+        role: "user"
+    }
+]
